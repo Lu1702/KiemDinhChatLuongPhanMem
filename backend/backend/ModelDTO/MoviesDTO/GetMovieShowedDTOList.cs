36ffd51b59d21eaf5f9c9ed9a188fe6663d692e6
Trần Anh Đức <157624643+Trananhduc19092005@users.noreply.github.com>
2025-07-30 19:17:59 +0700

    Fix Lỗi tìm kiếm lấy danh sách List , Thêm tính năng lấy lịch phim đang chiếu và sắp chiếu

---
+++ backend/backend/ModelDTO/MoviesDTO/GetMovieShowedDTOList.cs
@@ -0,0 +1,10 @@
+namespace backend.ModelDTO.MoviesDTO;
+
+public class GetMovieShowedDTOList
+{
+    public string MovieId { get; set; } = string.Empty;
+    
+    public string MovieName { get; set; } = string.Empty;
+    
+    public string MovieImage { get; set; } = string.Empty;
+}
