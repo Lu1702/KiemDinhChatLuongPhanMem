acf3fd4fe8c0c716feb481ca7a6384a988ec49ee
Trần Anh Đức <157624643+Trananhduc19092005@users.noreply.github.com>
2025-08-02 15:01:50 +0700

    Update_Them_data

--- backend/backend/ModelDTO/BookingsDTO/OrderRespond/OrderRespondDTO.cs
+++ backend/backend/ModelDTO/BookingsDTO/OrderRespond/OrderRespondDTO.cs
@@ -7,10 +7,66 @@ namespace backend.ModelDTO.Customer.OrderRespond
 {
     public class OrderRespondDTO
     {
-        public string? Error { get; set; }
+        public Dictionary<string, string> VnpayInfo { get; set; } = new Dictionary<string, string>();
 
-        public string ? VnpayURL { get; set; } 
+        public OrderRespondDTOInfo OrderRespondDtoInfo { get; set; } = null!;
+    }
 
-        public long TotalAmount { get; set; } 
+
+    public class OrderRespondDTOInfo
+    {
+        public string MovieName { get; set; } = string.Empty;
+
+        public string CinemaLocation { get; set; } = string.Empty;
+
+        public string RoomNumber { get; set; } = string.Empty;
+
+        public string VisualFormat { get; set; } = string.Empty;
+
+        public DateTime ShowedDate { get; set; }
+
+        public OrderRespondUserTypeWithPrices OrderRespondUserTypeWithPrices { get; set; } = null!;
+
+        public OrderRespondProductsInfoWithTotalPrice OrderRespondProductsInfoWithTotalPrice { get; set; } = null!;
+        
+        public double TotalPrice { get; set; }
+    }
+
+    public class OrderRespondUserTypeInfo
+    {
+        public string UserType { get; set; } = string.Empty;
+
+        public string SeatsNumber { get; set; } = string.Empty;
+
+        public double PriceTicket { get; set; }
+
+
+    }
+
+    public class OrderRespondUserTypeWithPrices
+    {
+        public List<OrderRespondUserTypeInfo> OrderRespondUserTypeInfos { get; set; } =
+            new List<OrderRespondUserTypeInfo>();
+
+        public double TotalPriceTicket { get; set; }
+    }
+
+
+
+    public class OrderRespondProductsInfo
+    {
+        public string ProductName { get; set; } = string.Empty;
+
+        public long Amount { get; set; } 
+
+        public Double ProductPrice { get; set; }
+    }
+
+    public class OrderRespondProductsInfoWithTotalPrice
+    {
+        public List<OrderRespondProductsInfo> OrderRespondProductsInfos { get; set; } =
+            new List<OrderRespondProductsInfo>();
+
+        public double TotalPrice { get; set; }
     }
 }
